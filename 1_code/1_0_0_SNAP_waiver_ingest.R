760375666e33eb6d83492c0544f650254e70fbf4
Inder <iamajumdar@gmail.com>
2025-11-16 12:42:56 -0600

    fixed waiver consolidation error

--- 1_code/1_0_0_SNAP_waiver_ingest.R
+++ 1_code/1_0_0_SNAP_waiver_ingest.R
@@ -1,13 +1,17 @@
-######################################################
-# ETL: SNAP Waivers 
 
-# Inder Majumdar
-######################################################
+#///////////////////////////////////////////////////////////////////////////////
+#----                              Preamble                                 ----
+# File name:        1_0_0_SNAP_waiver_ingest.R
+# Previous author:  -
+# Current author:   Inder Majumdar
+# Last Updated:    November 16, 2025
+# Description:     Consolidate year-state CSV files into one large file for further analysis
+#
+# Change log:       
+#///////////////////////////////////////////////////////////////////////////////
 
-##############
-#Config
-##############
 
+# Setup ------------------------------------------------------------------------
 
 # Load Packages
 library('tidyverse')
@@ -15,30 +19,33 @@ library('ellmer')
 library('gander')
 library('zoo')
 
-# Set the reference model for gander
-
-options(.gander_chat = ellmer::chat_ollama(model = "llama3.2"))
 
 # Set data path
 waiver_path = '/Users/indermajumdar/Library/CloudStorage/Box-Box/SNAP Dollar Entry/data/waivers/panels'
 
-##############
-#Load Data
-##############
+# Load Data
 
 waiver_data <- tibble()
 
 waiver_dir <- dir(waiver_path) 
 
 for (file in waiver_dir) { 
-  temp <- readxl::read_excel(paste(waiver_path, file, sep = "/")) #Why isn't this working?
+  temp <- readxl::read_excel(paste(waiver_path, file, sep = "/")) 
   waiver_data <- rbind(waiver_data, temp)
 }
 
+rm(temp)
 
-##############
-#Transform Waiver data into "Long" format: Month-year dummy creation
-##############
+
+# Data check: Is ENTIRE_STATE == 1 OR 0?
+
+waiver_data |>
+  filter(ENTIRE_STATE > 1) |>
+  select(YEAR, STATE) |>
+  unique()
+
+
+# Transform Waivers in to "long" format by date --------------------------------
 
 waiver_data$DATE_START <- as_date(waiver_data$DATE_START)
 
@@ -60,8 +67,8 @@ for (item in months_between) {
 # Fill columns based on dates data
 
 for (i in 1:nrow(waiver_data)) {
+  # Pull month-year sequence for each row
   months_between <- format(seq(from = as.yearmon(waiver_data$DATE_START[i]), to = as.yearmon(waiver_data$DATE_END[i]) , by = 1/12),"%b_%Y")
-  
   for (j in 1:length(months_between)) {
     temp <- months_between[j]
     waiver_data[i, temp] <- 1
@@ -75,5 +82,4 @@ file_path <- readLines("2_processed_data/processed_path.txt")[1]
 filesave <- paste(file_path, "/waiver_data_consolidated.csv", sep = "")
 
 write_csv(waiver_data, filesave)
-
 
